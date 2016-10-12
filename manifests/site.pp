3cc02fbe05d499cd790a9f0812c889a88b9913b2
benny1211 <bhernandez@misoenergy.org>
2016-10-12 13:05:23 -0400

    Update site.pp
    Updated content from github

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,13 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
+
+file { '/etc/motd':
+  ensure ==> file,
+  owner  ==> 'root',
+  group  ==> 'root',
+  mode   ==> '0440',
+  content ==> "This is pushed github!\n",
+}  
+ 
 }
