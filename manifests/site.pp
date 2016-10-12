da51aa5da653f070149cca425c75e79d5a2421eb
rfoxPuppetLab <rfox@carouselindustries.com>
2016-10-12 12:57:36 -0400

    Managing /etc/motd
    Added /etc/motd

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,11 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
+  file { '/etc/motd':
+   ensure => file,
+   owner => 'root',
+   group => 'root',
+   mode => '0644',
+   content => "While the Puppet learning VM is helpful, it's much more valuable to me having a live instructors experience and alternative examples presented in this class!\n",
+   }
 }
